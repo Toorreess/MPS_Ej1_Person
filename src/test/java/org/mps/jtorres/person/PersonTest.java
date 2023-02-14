84ca212217334cf9b3738ddcab3f9b305567a423
Jose Torres <jostorpos@uma.es>
2023-02-14 15:51:52 +0100

    PersonTest in progress

--- src/test/java/org/mps/jtorres/person/PersonTest.java
+++ src/test/java/org/mps/jtorres/person/PersonTest.java
@@ -1,26 +1,8 @@
 package org.mps.jtorres.person;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PersonTest {
-    Person pMale;
-    Person pFemale;
-    Person pNegativeAge;
-    Person pIncorrectGender;
-    @BeforeAll
-    void setup(){
-        pMale = new Person("test", 20, "male");
-        pFemale = new Person("test", 20, "female");
-        pNegativeAge = new Person("AgeError", -2, "male");
-        pIncorrectGender = new Person("GenderError", 60, "WrongGender");
-    }
-
-    @AfterEach
-    void shutdown() {
-        pMale = null;
-        pFemale = null;
-        pNegativeAge = null;
-        pIncorrectGender = null;
-    }
 }
